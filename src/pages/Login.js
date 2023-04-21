2ccac2408c39abf41a990ad642a8fcf2791e47a8
beetz1985 <benjamintoulson@hotmail.co.uk>
2023-04-22 00:01:15 +0100

    Little details

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
 
     function handleSubmit(e) {
         e.preventDefault()
-        postRequest(`h${API_BASE_URL}/april/jwt/api/login`, {email, password})
+        postRequest(`${API_BASE_URL}/april/jwt/api/login`, {email, password})
 
         //Hit API Authentication Endpoint
     }
